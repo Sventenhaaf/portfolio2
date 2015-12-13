599b45835dd6a68b67c00ebb5d1f987941ab3ff0
Sven ten Haaf <sventenhaaf@hotmail.com>
2015-12-13 22:48:36 +0100

    quicksort and bubblesort done

--- bubblesort.js
+++ bubblesort.js
@@ -1,58 +1,78 @@
-var width = 960,
-    height = 500;
+(function() {
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(32," + (height / 2) + ")");
+  var arr = [];
+  for (var idxx = 0; idxx < 80; idxx++) { arr.push(idxx); }
 
-function update(data, indices) {
-
-  var text = svg.selectAll("text")
-      .data(data, function(d) { return d; });
-
-  text.attr("class", "update")
-    .style("fill", function(d, i) { return indices.indexOf(i) == -1 ? "#666" : "red" })
-    .transition()
-      .attr("y", function(d, i) { return indices.indexOf(i) == -1 ? 0 : 50 })
-    .transition()
-      .attr("x", function(d, i) { return i * 32; })
-    .transition()
-      .attr("y", 0);
-
-  text.enter().append("text")
-      .attr("class", "enter")
-      .attr("dy", ".35em")
-      .attr("x", function(d, i) { return i * 32; })
-      .text(function(d) { return d; })
-      .attr("y", 0)
-      .style("fill-opacity", 1);
-}
-
-// - - - - BUBBLE SORT ALGORITHM - - - -
-var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-for(var j, x, i = a.length; i; j = Math.floor(Math.random() * i),
-  x = a[--i], a[i] = a[j], a[j] = x);
-
-
-var swapped = true;
-var i = 0;
-var indices = [0, 1];
-update(a, indices);
-
-var myInterval = setInterval(function() {
-  if (a[i] > a[i+1]) {
-    var temp = a[i]; a[i] = a[i+1]; a[i+1] = temp; swapped = true;
+  for(var j, x, i = arr.length; i; j = Math.floor(Math.random() * i),
+    x = arr[--i], arr[i] = arr[j], arr[j] = x);
+  // Using D3 making an update function for a step in sorting algorithm
+  var width = 1600,
+      height = 250,
+      stepDuration = 50;
+  if (Math.max.apply(null, arr) < 10) {
+    var charSize = width / (1.5 * arr.length);
+    var dx = charSize;
   }
-  indices = [i, i+1];
-  update(a, indices);
-  if (i > a.length - 3 && swapped) { i = 0; swapped = false; }
-  else { i++ };
-  // SLOPPY - making sure no numbers stay red
-  if (i > a.length - 1) {
-    clearInterval(myInterval);
-    update(a, [a.length + 1, a.length + 2])
+  else {
+    var charSize = width / (3 * arr.length);
+    var dx = 2.5 * charSize;
   }
-}, 750);
+
+  var svg = d3.select(".bubblesort").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .style("font", "bold " + 1.5*charSize + "px monospace")
+      .attr("transform", "translate(32," + (height / 2) + ")");
+
+  function update(data, indices) {
+
+    var text = svg.selectAll("text")
+        .data(data, function(d) { return d; });
+
+    text.attr("class", "update")
+      .style("fill", function(d, i) { return indices.indexOf(i) == -1 ? "#666" : "red" })
+      .transition()
+      .duration(stepDuration)
+        .attr("y", function(d, i) { return indices.indexOf(i) == -1 ? 0 : charSize * 1.2 })
+      .transition()
+        .duration(stepDuration)
+        .attr("x", function(d, i) { return i * dx; })
+      .transition()
+        .duration(stepDuration)
+        .attr("y", 0);
+
+    text.enter().append("text")
+        .attr("class", "enter")
+        .attr("dy", ".35em")
+        .attr("x", function(d, i) { return i * dx; })
+        .text(function(d) { return d; })
+        .attr("y", 0)
+        .style("fill-opacity", 1);
+  }
+  // - - - - BUBBLE SORT ALGORITHM - - - -
+
+
+
+  var swapped = true;
+  var i = 0;
+  var indices = [0, 1];
+  update(arr, indices);
+
+  var myInterval = setInterval(function() {
+    if (arr[i] > arr[i+1]) {
+      var temp = arr[i]; arr[i] = arr[i+1]; arr[i+1] = temp; swapped = true;
+    }
+    indices = [i, i+1];
+    update(arr, indices);
+    if (i > arr.length - 3 && swapped) { i = 0; swapped = false; }
+    else { i++ };
+    // SLOPPY - making sure no numbers stay red
+    if (i > arr.length - 1) {
+      clearInterval(myInterval);
+      update(arr, [arr.length + 1, arr.length + 2])
+    }
+  }, 3 * stepDuration);
+
+
+})();
--- script.js
+++ script.js
@@ -30,8 +30,34 @@ function update(data, indices) {
       .style("fill-opacity", 1);
 }
 
+// -- start new setINterval code
+var count = 0,
+    time = 1000,
+    intId;
+function invoke(){
 
-var arr = [6, 5, 7, 4, 9, 3, 8, 1, 2];
+    intId = setInterval(function(){
+        count += 1;
+        console.log(count);
+        if(count % 2 === 0) // now i need to change my time
+        {
+           time += 2000; //some new value
+           intId = window.clearInterval(intId);
+           invoke();
+        }
+    }, time);
+
+}
+
+invoke();
+
+
+
+
+// -- Quicksort code
+
+
+var arr = [0, 7, 3, 5, 9, 1, 4, 8, 2, 6];
 var indicesOld = [0, 1];
 var indicesNew = [0, 1];
 
@@ -50,8 +76,6 @@ function qsort(arr, ret)
 
     intervalId = setInterval(function() {
       if (stack.length || busy) {
-        console.log("tl");
-        if(typeof tl !== "undefined") console.log(tl);
         if(busy) { }
         else {
           temp = stack.pop(), tl = temp.length;
@@ -121,161 +145,16 @@ function qsort(arr, ret)
 
 
 qsort(arr, function(sorted) {
-  console.log("sorted:");
   console.log(sorted);
 });
 
 
 
 
-//
-// console.log('temp:');
-// console.log(temp);
-// console.log('pivot:');
-// console.log(pivot);
-// console.log('temp from i to end:');
-// console.log(temp.slice(i))
-// console.log('stack:');
-// for (var idx = 0; idx < stack.length; idx++) {
-//   console.log(stack[idx]);
-// }
-// console.log('sorted:');
-// console.log(sorted);
-// console.log('left:');
-// console.log(left);
-// console.log('right:');
-// console.log(right);
-// console.log("TOTAL");
-// console.log(tot);
-// console.log('indices');
-// console.log(indices);
-// console.log('---------');
 
 
 
-//
-//
-//
-//
-// var array = [3, 1, 4, 6, 2, 5];
-//
-//
-//
-// var sorted = [];
-// var right = [];
-// var a = array;
-// var totalA = a;
-//
-// var i = 0;
-// var j = 1;
-//
-// var indices = [0, 1];
-// update(totalA, indices);
-//
-// var myInterval = setInterval(function() {
-//
-//
-//
-//   //original code
-//     if (a[i] > a[j]) {
-//       a = a.slice(0, i).concat(a.slice(j, j+1)).concat(a.slice(i, j)).concat(a.slice(j+1));
-//       indices = [i, i+1];
-//       i++; j++;
-//     }
-//     else {
-//       indices = [i, j];
-//       j++; }
-//
-//
-//
-//
-//
-//     totalA = sorted.concat(a);
-//     console.log(totalA);
-//     console.log(sorted);
-//     console.log(a);
-//     console.log('-------')
-//     update(totalA, [(indices[0] + sorted.length), (indices[1] + sorted.length)]);
-//
-//
-//
-//     if (j === a.length) {
-//       if (i < 2) {
-//         sorted = sorted.concat(a.slice(0, i+1));
-//         a = a.slice(i+1);
-//         i = 0;
-//         j = 1;
-//       }
-//       else {
-//         right.push(i);
-//       }
-//     }
-//
-// }, 2750)
-//
-//
-//
-// //
-// // // - - - - QUICK SORT ALGORITHM - - - -
-// // var array = [3, 1, 5, 2, 6, 4];
-// //
-// // // 3, 1, 5, 2, 6, 4
-// // // - left: [] -- the already sorted
-// // // - active: [6, 5, 4, 3, 2, 1] --- a
-// // // - right: [] -- still to sort
-// // // rightindices = []
-// //
-// // // one sort loop on a
-// //
-// //
-// //
-// //
-// //
-// // var a = array;
-// //
-// // var swapped = true;
-// // var i = 0; // make this one random
-// // var j = 1;
-// // var indices = [0, 1];
-// // update(a, indices);
-// //
-// // var myInterval = setInterval(function() {
-// //   j = i
-// //
-// //
-// //
-// //
-// //
-// //
-// //
-// //   // console.log(typeof j)
-// //   if (typeof j === "undefined" || j === i) { j = i + 1; }
-// //   indices = [i, j]
-// //   if (a[i] > a[j]) {
-// //     console.log(a.slice(0, i), a.slice(j, j+1), a.slice(i, j), a.slice(j+1))
-// //     a = a.slice(0, i).concat(a.slice(j, j+1)).concat(a.slice(i, j)).concat(a.slice(j+1));
-// //     i++;
-// //   }
-// //   else {
-// //     j++
-// //     console.log("j+")
-// //    }
-// //
-// //   // if (a[i] > a[i+1]) {
-// //   //   var temp = a[i]; a[i] = a[i+1]; a[i+1] = temp; swapped = true;
-// //   // }
-// //   // indices = [i, j];
-// //   console.log(indices);
-// //   update(a, indices);
-// //
-// //
-// //
-// //   // if (a.slice(0, i) <)
-// //   // if (j < a.length - 1) { j++; }
-// //   // else { i++ };
-// //   // SLOPPY - making sure no numbers stay red
-// //   // if (i > a.length - 1) {
-// //   //   clearInterval(myInterval);
-// //   //   update(a, [a.length + 1, a.length + 2])
-// //   // }
-// // }, 3750);
+
+
+
+//---------------------------------------------------------------------------
