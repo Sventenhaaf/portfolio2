ba85f8d8f94f9ecfb46e1c5fd23de78bc5275884
Sven ten Haaf <sventenhaaf@hotmail.com>
2015-12-17 13:25:44 +0100

    now starting work on units

--- motion.js
+++ motion.js
@@ -1,52 +1,77 @@
+// Unit settings
+var excitation  = 1;    // excitation       [m]
+var mass        = 10;   // mass             [kg]
+
 // System Settings
-var stiffness   = 0.02; // spring stiffness
-var damping     = 0.02; // system damping
-var mass        = 10;   // mass of ball
+var stiffness   = 0.0040;   // spring stiffness [kg/s^2]
+var damping     = 0;    // system damping   [kg/s]
 
 // Representation settings
 var totalWidth  = 1300; // total width of screen
-var totalHeight = 500;  // total height of screen
+var ctrlHeight  = 50;   // total height of control panel
+var mtnHeight   = 500;  // total height of motion simulation screen
+var stgHeight   = 300;  // total height of settings screen
+var margin      = {top: 20, right: 20, bottom: 20, left: 20}; // setting screen
+    width       = totalWidth - margin.left - margin.right,
+    height      = mtnHeight - margin.top - margin.bottom;
 var timeStep    = 20;   // time step between each calculation
-var offsetX     = 60;   // distance system is from right edge
-var offsetY     = 150;  // distance system is from ceiling
-var ntrHeight   = 250;  // height at which spring force is 0
+var offsetX     = 50;   // distance system is from right edge
+var offsetY     = 0;  // distance system is from ceiling
+var ntrHeight   = 150;  // height at which spring force is 0
 var t           = 0;    // initial setting of time variable t
 var data        = [];   // initial setting of data array to represent line graph
 var timeScale   = 10;   // scale with which time is projected to x direction
-var margin = {top: 20, right: 20, bottom: 20, left: 20}, // setting screen
-    width = totalWidth - margin.left - margin.right,
-    height = totalHeight - margin.top - margin.bottom;
+
 
 // Boundary conditions
 var velocity  = 0;
 // var startPosition = 1;
 
-
-
-
 var svg = d3.select(".motion").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  // .append("g")
-  // .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-  .style("display", "flex")
-  .style("align-items", "center")
-  .style("justify-content", "center")
+  .attr("width", totalWidth)
+  .attr("height", mtnHeight)
+  .style({
+    "display"         : "block",
+    "margin"          : "auto",
+    "align-items"     : "center",
+    "justify-content" : "center",
+    "border"          : "1px solid black"
+  })
 
-// var controlPanel = d3.select(.motion).append("svg")
+var controlPanel = d3.select(".controlPanel").append("svg")
+  .attr("width", totalWidth)
+  .attr("height", ctrlHeight)
+  .style({
+    "display"         : "block",
+    "margin"          : "auto",
+    "align-items"     : "center",
+    "justify-content" : "center",
+    "border"          : "1px solid black"
+  })
 
-var startSquare = svg
+  var settings = d3.select(".settings").append("svg")
+    .attr("width", totalWidth)
+    .attr("height", stgHeight)
+    .style({
+      "display"         : "block",
+      "margin"          : "auto",
+      "align-items"     : "center",
+      "justify-content" : "center",
+      "border"          : "1px solid black"
+    })
+
+var startSquare = controlPanel
   .append("rect")
   .attr("x", 300)
-  .attr("y", 100)
+  .attr("y", 10)
   .attr("height", 30)
   .attr("width", 30)
   .attr("onclick", "startAnimation()")
   .style("fill", "#ccc")
 
-var stopSquare = svg.append("rect")
+var stopSquare = controlPanel.append("rect")
   .attr("x", 400)
-  .attr("y", 100)
+  .attr("y", 10)
   .attr("height", 30)
   .attr("width", 30)
   .attr("onclick", "stopAnimation()")
@@ -58,7 +83,7 @@ var spring = svg.append("line")
   .attr("y1", 0)
   .attr("x2", width - offsetX)
   .attr("y2", offsetY)
-  .attr("stroke-width", 5)
+  .attr("stroke-width", 2)
   .attr("stroke", "rgb(255, 0, 0)")
   .attr("id", "spring")
 
@@ -81,7 +106,7 @@ var lineGraph = svg.append("path")
   .attr("stroke", "blue")
   .attr("stroke-width", 2)
   .attr("fill", "none")
-  .style("stroke-dasharray", "5, 5")
+  // .style("stroke-dasharray", "5, 15")
 
 
 // - - OTHER CODE - -
@@ -106,8 +131,8 @@ function setNewY() {
   var y = parseFloat(document.getElementById("ball").getAttribute("cy"));
   y += velocity;
   velocity -= damping * velocity;
-  console.log(velocity)
   velocity -= stiffness * (y - ntrHeight - offsetX);
+  console.log(y);
   return y;
 }
 
@@ -160,3 +185,4 @@ function animate() {
 // external (resonating) force
 // SI Units
 // axis and scales
+// pop-up saying: Looks like your system reached steady-state. Do you want to continue?
